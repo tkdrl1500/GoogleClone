f103f023cfeb438c82526d1e71502a9d009e4f73
tkdrl1500 <70390705+tkdrl1500@users.noreply.github.com>
2021-05-19 17:05:20 +0900

    모달창 나머지 보완

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const modalFan = document.querySelector(".modalFan_btn");
 const modal = document.querySelector(".modalBack");
-const modalOut = document.querySelector(".modalOut");
+const modalOut = document.querySelector(".modalBottom__out");
 const body = document.querySelector("body");
 
 function onClick() {
@@ -16,12 +16,14 @@ modalFan.addEventListener("click", (e) => {
   if (onbtn) {
     onClick();
     body.style.overflow = "hidden";
+    body.style.background = "rgba(0, 0, 0, 0.8)";
   }
 });
 
 modalOut.addEventListener("click", (e) => {
   const offBtn = e.target.className;
-  if (offBtn === "modalOut") {
+  if (offBtn === "modalBottom__out hoverBtn") {
     offClick();
+    body.style.background = "rgb(232, 233, 235)";
   }
 });
